c446749bf2cf8a02def1a40ca14b2e79f24c7653
Neeha-shirin <neehasherin987@gmail.com>
2025-07-23 11:35:26 +0530

    Update models.py

--- config/store/models.py
+++ config/store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
+//
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
